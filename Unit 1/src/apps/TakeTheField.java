180cbc604a035fd3f1187c970830489c3b421148
Yogesh Thambidurai <gyogesh2015@outlook.com>
2021-10-03 11:44:52 -0400

    Update apps to use correct imports Now imports SuperRobot instead of child class

--- Unit 1/src/apps/TakeTheField.java
+++ Unit 1/src/apps/TakeTheField.java
@@ -3,11 +3,11 @@ package apps;
 
 import edu.fcps.karel2.Display;
 
-import robots.SuperRobotV2;
+import robots.SuperRobot;
 
 public class TakeTheField {
 
-    public static void enterField(SuperRobotV2 player) {
+    public static void enterField(SuperRobot player) {
 
         player.moveX(4);
         player.turnRight();
@@ -20,19 +20,19 @@ public class TakeTheField {
         Display.openWorld("lib/Jkarel Start Files/maps/arena.map");
         Display.setSize(10, 10);
 
-        new SuperRobotV2(2, 7, Display.EAST);
+        new SuperRobot(2, 7, Display.EAST);
 
         /*
          * I spent forever here trying to create a list to hold these guys so I could
          * just loop over it but I forgot my Java lists so I ended up wasting ~30 min on
          * this. Enjoy!
          */
-        SuperRobotV2 one = new SuperRobotV2(1, 1, Display.NORTH);
-        SuperRobotV2 two = new SuperRobotV2(1, 1, Display.NORTH);
-        SuperRobotV2 three = new SuperRobotV2(1, 1, Display.NORTH);
-        SuperRobotV2 four = new SuperRobotV2(1, 1, Display.NORTH);
-        SuperRobotV2 five = new SuperRobotV2(1, 1, Display.NORTH);
-        SuperRobotV2 six = new SuperRobotV2(1, 1, Display.NORTH);
+        SuperRobot one = new SuperRobot(1, 1, Display.NORTH);
+        SuperRobot two = new SuperRobot(1, 1, Display.NORTH);
+        SuperRobot three = new SuperRobot(1, 1, Display.NORTH);
+        SuperRobot four = new SuperRobot(1, 1, Display.NORTH);
+        SuperRobot five = new SuperRobot(1, 1, Display.NORTH);
+        SuperRobot six = new SuperRobot(1, 1, Display.NORTH);
 
         enterField(one);
         enterField(two);
