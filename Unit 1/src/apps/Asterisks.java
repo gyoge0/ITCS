c653e76042f0170390417d33cfc2a3c8d4f4f484
Yogesh Thambidurai <gyogesh2015@outlook.com>
2021-10-05 11:58:05 -0400

    Modified Asterisks to make use of nested for loops

--- Unit 1/src/apps/Asterisks.java
+++ Unit 1/src/apps/Asterisks.java
@@ -4,6 +4,16 @@ package apps;
 
 public class Asterisks {
 
+    private static String repeatChar(int amount, char inputChar) {
+
+        String returnString = "";
+        for (int i = 1; i <= amount; i++) {
+            returnString = returnString + inputChar;
+        }
+
+        return returnString;
+    }
+
     public static void main(String[] args) {
 
         partA();
@@ -21,7 +31,7 @@ public class Asterisks {
         System.out.println("Part A\n");
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println("*".repeat(i));
+            System.out.println(repeatChar(i, '*'));
         }
 
     }
@@ -33,7 +43,7 @@ public class Asterisks {
         System.out.println("\nPart B\n");
 
         for (int i = 10; i >= 1; i--) {
-            System.out.println("*".repeat(i));
+            System.out.println(repeatChar(i, '*'));
         }
 
     }
@@ -45,7 +55,7 @@ public class Asterisks {
         System.out.println("\nPart C\n");
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println(" ".repeat(10 - i) + "*".repeat(i));
+            System.out.println(repeatChar(10 - 1, ' ') + repeatChar(i, '*'));
         }
 
     }
@@ -57,7 +67,7 @@ public class Asterisks {
         System.out.println("\nPart D\n");
 
         for (int i = 10; i >= 1; i--) {
-            System.out.println(" ".repeat(10 - i) + "*".repeat(i));
+            System.out.println(repeatChar(10 - 1, ' ') + repeatChar(i, '*'));
         }
 
     }
@@ -69,11 +79,11 @@ public class Asterisks {
         System.out.println("\nPart E\n");
 
         for (int i = 1; i <= 13; i += 2) {
-            System.out.println(" ".repeat((13 - i) / 2) + "*".repeat(i));
+            System.out.println(repeatChar((13 - i) / 2, ' ') + repeatChar(i, '*'));
         }
 
         for (int i = 13; i >= 1; i -= 2) {
-            System.out.println(" ".repeat((13 - i) / 2) + "*".repeat(i));
+            System.out.println(repeatChar((13 - i) / 2, ' ') + repeatChar(i, '*'));
         }
 
     }
