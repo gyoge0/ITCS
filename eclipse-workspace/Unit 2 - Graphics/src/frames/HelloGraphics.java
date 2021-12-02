7dd58fc852c14519270dece621068680929e7d8c
Yogesh Thambidurai <gyogesh2015@outlook.com>
2021-12-02 12:47:15 -0500

    updates

--- eclipse-workspace/Unit 2 - Graphics/src/frames/HelloGraphics.java
+++ eclipse-workspace/Unit 2 - Graphics/src/frames/HelloGraphics.java
@@ -5,44 +5,15 @@ package frames;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import java.awt.*;
+import javax.swing.*;
 
 public class HelloGraphics extends JPanel {
 
     @Override
     public void paintComponent(Graphics goon) {
 
-        goon.setColor(Color.RED);
-        goon.fillOval(100, 100, 100, 100);
-
-        goon.setColor(new Color(69, 69, 69));
-        goon.fillRect(420, 420, 69, 69);
-
-        goon.setColor(Color.darkGray);
-        goon.drawLine(0, 0, 500, 0);
-
-        goon.setColor(Color.lightGray);
-        goon.drawLine(0, 0, 0, 500);
-
-        goon.setFont(new Font("JetBrains Mono", Font.BOLD, 75));
-        goon.setColor(Color.BLACK);
-
-        ArrayList<String> words = new ArrayList<>(Arrays.asList("ITS ABOUT DRIVE", "ITS ABOUT POWER",
-                "WE STAY HUNGRY", "WE DEVOUR", "PUT IN THE WORK", "PUT IN THE HOURS", "AND TAKE WHATS OURS"));
-        for (int i = 1; i < words.size(); i++) {
-            goon.drawString(words.get(i), 10, (i + 1) * 100);
-        }
-
-        goon.setColor(new Color(123, 123, 123));
-        for (int i = 0; i < 3; i++) {
-            goon.fillOval(i * 10, i * 10, 10, 10);
-        }
+        JLabel
 
     }
 
