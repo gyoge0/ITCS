0505791245b70ff564253ceea9c53610bd116f57
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-02-05 16:25:24 -0500

    Format

--- Unit 2 - Graphics/src/main/java/com/gyoge/itcsgraphics/animations/Animation.java
+++ Unit 2 - Graphics/src/main/java/com/gyoge/itcsgraphics/animations/Animation.java
@@ -14,15 +14,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-import javax.swing.WindowConstants;
 
 /**
  * Animation
