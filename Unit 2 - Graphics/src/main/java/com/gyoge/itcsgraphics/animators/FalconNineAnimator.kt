86c2460a1c673d9b5b841c81e29518ff5b912664
Yogesh Thambidurai <gyogesh2015@outlook.com>
2022-02-16 21:33:23 -0500

    More flexible values

--- Unit 2 - Graphics/src/main/java/com/gyoge/itcsgraphics/animators/FalconNineAnimator.kt
+++ Unit 2 - Graphics/src/main/java/com/gyoge/itcsgraphics/animators/FalconNineAnimator.kt
@@ -23,7 +23,30 @@ class FalconNineAnimator(
     height: Int = 100,
     /** Change in time for each frame. */
     private val dT: Duration = ChronoUnit.SECONDS.duration,
+    /** Different masses. */
+    private val masses: HashMap<Masses, Double> = defaultMasses
 ) : Animator {
+
+    constructor(x: Int, y: Int, width: Int, height: Int, dT: Duration) : this(
+        x,
+        y,
+        width,
+        height,
+        dT,
+        defaultMasses
+    )
+
+    /** Enum containing keys for the masses hashmap. */
+    enum class Masses {
+        PHASE_1_STARTING,
+        PHASE_1_ENDING,
+        PHASE_1_FUEL,
+        PHASE_1_THRUST,
+        PHASE_2_STARTING,
+        PHASE_2_ENDING,
+        PHASE_2_FUEL,
+        PHASE_2_THRUST,
+    }
 
     val drawable = Spaceship()
 
@@ -36,10 +59,10 @@ class FalconNineAnimator(
 
 
     /** Velocity of the spaceship. */
-    private var vel = 0.0
+    var vel = 0.0
 
     /** Acceleration of the spaceship. */
-    private var accel = 0.0
+    var accel = 0.0
 
     /** Altitude of the spaceship. */
     var alt = 0.0
@@ -47,14 +70,22 @@ class FalconNineAnimator(
     /** Time elapsed. */
     var time: Duration = Duration.ZERO
 
+    /** Current mass of the rocket. */
+    var mass: Double = 0.0
+
+    /** Current net force acting on the object. */
+    var netForce: Double = 0.0
+
     /** Assuming the frontal area is the width squared of the spaceship. */
     private val frontalArea = drawable.width.toDouble().pow(2.0)
+
 
     fun getStage(): Int {
         // return 2 if time.seconds is greater than 162, otherwise return 1
         return if (time.seconds > 162) 2 else 1
 
     }
+
 
     /** Do the math and then update the drawable. */
     override fun getDrawable(params: HashMap<String, Any>): Drawable {
@@ -63,33 +94,60 @@ class FalconNineAnimator(
             return drawable
         }
 
-        var mass: Double = 541300 - (398900 / 162 * (sec)).toDouble()
-        var thrust = 6806000.0
+        var thrust = 0.0
+        var fuel = 0.0
+
+        when (getStage()) {
+            1 -> {
+                val starting = masses[Masses.PHASE_1_STARTING] ?: 541300.0
+                fuel = masses[Masses.PHASE_1_FUEL] ?: 398900.0
+                thrust = masses[Masses.PHASE_1_THRUST] ?: 6806000.0
+                mass = starting - (fuel / 162 * sec)
+            }
+            2 -> {
+                val starting = masses[Masses.PHASE_2_STARTING] ?: 96570.0
+                fuel = masses[Masses.PHASE_2_FUEL] ?: 92670.0
+                thrust = masses[Masses.PHASE_2_THRUST] ?: 934000.0
+                mass = starting - ((fuel / (397 - 162)) * (sec.toDouble() - 162))
+            }
+        }
+
 
         if (sec.toDouble() > 162) {
-            mass = 96570 - ((92670 / (397 - 162)) * (sec.toDouble() - 162))
-            thrust = 934000.0
+            mass = fuel - ((fuel / (397 - 162)) * (sec.toDouble() - 162))
         }
 
         val fG = mass * 6.67e-11 * 5.978e24 / ((6.38e6 + alt).pow(2.0))
 
         val fD = (0.5) * 0.295 * frontalArea * getDensity(alt) * vel.pow(2.0)
 
-        val net = thrust - fG - fD
+        netForce = thrust - fG - fD
 
-        accel = net / mass
+        accel = netForce / mass
         vel += accel * dT.toNanos().toDouble() / 1e9
         alt += vel * dT.toNanos().toDouble() / 1e9
 
         time = time.plus(dT)
 
-        drawable.y = (alt * 100000 / (params["HEIGHT"] as Int)) - drawable.height
+        drawable.y =
+            params["HEIGHT"] as Int - alt * params["HEIGHT"] as Int / 300000.0 - drawable.height
 
         return drawable
     }
 
 
     private companion object {
+
+        val defaultMasses: HashMap<Masses, Double> = hashMapOf(
+            Masses.PHASE_1_STARTING to 541300.0,
+            Masses.PHASE_1_ENDING to 142400.0,
+            Masses.PHASE_1_FUEL to 398900.0,
+            Masses.PHASE_1_THRUST to 6806000.0,
+            Masses.PHASE_2_STARTING to 96570.0,
+            Masses.PHASE_2_ENDING to 3900.0,
+            Masses.PHASE_2_FUEL to 92670.0,
+            Masses.PHASE_2_FUEL to 934000.0,
+        )
 
         /**
          * Calculates the air density at a given altitude.
@@ -103,6 +161,7 @@ class FalconNineAnimator(
         fun getDensity(alt: Double): Double {
             return 1.2787 * Math.E.pow(-0.000114616 * alt)
         }
+
 
     }
 }
